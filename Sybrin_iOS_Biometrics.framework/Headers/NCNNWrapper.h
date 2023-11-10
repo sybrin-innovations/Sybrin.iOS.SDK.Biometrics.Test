bff4078806ff45969a6c2934b63127ba63290cc9
JenkinsCI <releasemanager@sybrin.co.za>
2023-11-10 13:30:31 +0200

    [CI] Updated Sybrin.iOS.SDK.Biometrics.Test to version 1.4.17.beta

---
+++ Sybrin_iOS_Biometrics.framework/Headers/NCNNWrapper.h
@@ -0,0 +1,26 @@
+//
+//  NCNNWrapper.h
+//  FunAlbum
+//
+//  Created by Jinbin Xie on 11/9/18.
+//  Copyright © 2018年 Jinbin Xie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NCNNWrapper : NSObject
+
++(nonnull NSString *)openCVVersionString;
++(nonnull UIImage *)cvtColorBGR2GRAY:(nonnull UIImage *)image;
++(void)initialize;
++(bool)reSampleFace:(nonnull UIImage *)rawImage;
+
++(nonnull UIImage *)detectFace: (nonnull UIImage *)rawImage;
+
++(double)returnFaceDetectionPrediction: (nonnull UIImage *)rawImage;
+
++(nullable NSArray *)getCurrentFeatureSample;
++(bool)setCurrentFeatureSample: (nonnull NSArray *)featureSetNSArray;
+
+@end
---
+++ Sybrin_iOS_Biometrics.framework/Headers/Sybrin_iOS_Biometrics.h
@@ -0,0 +1,21 @@
+//
+//  Sybrin_iOS_Biometrics.h
+//  Sybrin.iOS.Biometrics
+//
+//  Created by Nico Celliers on 2020/09/29.
+//  Copyright © 2020 Sybrin Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sybrin_iOS_Biometrics.
+FOUNDATION_EXPORT double Sybrin_iOS_BiometricsVersionNumber;
+
+//! Project version string for Sybrin_iOS_Biometrics.
+FOUNDATION_EXPORT const unsigned char Sybrin_iOS_BiometricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sybrin_iOS_Biometrics/PublicHeader.h>
+
+//#import "OpenCVWrapper.h"
+#import "NCNNWrapper.h"
+
